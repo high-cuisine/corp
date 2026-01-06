5764e9976b8e60a52dcc416ba20ca60f8543e76e
Александр Носиков <aleksandrnosikov@MacBook-Pro-Aleksandr.local>
2026-01-06 23:45:37 +0400

    fix

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   // Отключаем Turbopack, если он вызывает проблемы
   // webpack: (config) => config,
 };
