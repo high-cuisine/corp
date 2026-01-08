c52a431183764d6ca5cbb7a104b7f8e32152713b
Александр Носиков <aleksandrnosikov@MacBook-Pro-Aleksandr.local>
2026-01-09 00:18:24 +0400

    something

--- server/src/payment/payment.module.ts
+++ server/src/payment/payment.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
+import { PaymentRepository } from './repository/payment.repository';
+import { UsersModule } from '../users/users.module';
+import { TelegramModule } from '../../lib/infrastructere/telegram/telegram.module';
 
 @Module({
-    imports: [],
+    imports: [UsersModule, TelegramModule],
     controllers: [PaymentController],
-    providers: [PaymentService],
+    providers: [PaymentService, PaymentRepository],
 })
 export class PaymentModule {}
